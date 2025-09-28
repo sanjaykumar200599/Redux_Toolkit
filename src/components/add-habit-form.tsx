da4251246ee12dd0bebb48e30dc54af6a39f18f5
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-09-28 22:49:07 +0530

    added the habit

--- src/components/add-habit-form.tsx
+++ src/components/add-habit-form.tsx
@@ -1,13 +1,26 @@
 import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import type { AppDispatch } from '../store/store';
+import { addHabit } from '../store/habit-slice';
 
 const AddHabitForm : React.FC = () => {
     const [name,setName]=useState<string>("");
     const [frequency,setFrequency]= useState<"daily" | "weekly">("daily");
 
-  
+    const dispatch=useDispatch<AppDispatch>();
+
     const handleSubmit=(e: React.FormEvent)=>{
         e.preventDefault();
+        if(name.trim()){
+          dispatch(
+            addHabit({
+              name,
+              frequency,
+            })
+          );
+          setName("");
+        }
     };
 
   return <form onSubmit={handleSubmit}>
