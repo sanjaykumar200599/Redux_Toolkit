31155ebe4a727c163e4b9f52069543b63f442b65
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-09-28 11:59:58 +0530

    Added the redux stores

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import habitsReducer from './habit-slice';
 
-export default configureStore({
-  reducer: {},
+const store=configureStore({
+  reducer: {
+    habits:habitsReducer
+  },
 })
+
+export type  Rootstate= ReturnType<typeof store.getState>;
+export type AppDispatch=typeof store.dispatch;
+export default store;
